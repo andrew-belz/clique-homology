75c10790f1d6d924c1eeb1c8c221a42da9649dce
andrew-belz <andrewbelzjr@gmail.com>
2026-05-05 09:42:27 -0600

    tidying repository

--- cpp/src/internal/maximal_cliques.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "data_types.hpp"
-
-inline CliqueList find_maximal_cliques(const Graph& graph) {
-    CliqueList list;
-    return list;
-}
--- cpp/src/main/betti.cpp
+++
@@ -1,8 +0,0 @@
-#include <vector>
-#include "temp_graph.hpp"
-
-using namespace std;
-
-vector <int> betti(const vector <vector <int>>& graph, const vector <int>& coloring) {
-    // Implement betti vecor calculation logic here
-}
