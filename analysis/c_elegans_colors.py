7a42a45b2da901cf3b340ec3101c9582f09ce57d
andrew-belz <andrewbelzjr@gmail.com>
2026-05-06 10:49:52 -0600

    updated scripts to obtain functional group labels and synapse edges

--- analysis/c_elegans_colors.py
+++ analysis/c_elegans_colors.py
@@ -1,14 +1,35 @@
-import csv
+import json
 
 def c_elegans_colors():
+    """
+    Return the neurons and their functional groupings. 
+    """
     
-    with open("c_elegans_data\maleChemicalFunctionGroupColoring.csv", "r") as file:
-        reader = csv.reader(file)
+    with open(r"c_elegans_data\neuron_indices.json", "r") as file:
+        data = json.load(file)
 
-    neurons = []
-    group = []
-    for row in reader:
-        neurons.append(row[0])
-        group.append(row[1])
+        neurons = [] # neuron names
+        I = [] # associated indices
+        groups = [] # neuron node labels
+
+        for neuron in data:
+
+            # make sure the neuron has a valid group assigned
+            try:
+                i = data[neuron][0]
+                group = data[neuron][1]
+
+                neurons.append(neuron)
+                I.append(i)
+                groups.append(group)
+
+            except:
+                print(f"{neuron} has no assigned group.")
+
+        return neurons, I, groups
+
+        
+if __name__ == "__main__":
+    print(c_elegans_colors())
 
     
