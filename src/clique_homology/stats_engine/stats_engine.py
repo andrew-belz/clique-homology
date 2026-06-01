55e42fe5e828649cb2fdc5add3298cd6cd1a186a
andrew-belz <andrewbelzjr@gmail.com>
2026-06-01 12:22:20 -0600

    changed code to look at individual betti numbers

--- src/clique_homology/stats_engine/stats_engine.py
+++ src/clique_homology/stats_engine/stats_engine.py
@@ -9,22 +9,39 @@ class StatsEngine:
     def __init__(self, G:Graph, labels:list[str]):
         self.G = G
         self.labels = labels
+        self.obs_betti = None
+        self.null_betti = None
+        self.p = None
+        self.obsD2 = None
+        self.nullD2 = None
 
 
     def observed_betti(self) -> np.ndarray:
-        return betti_numbers(G=self.G, colors=self.labels)
+        self.obs_betti = betti_numbers(G=self.G, colors=self.labels)
+        return self.obs_betti
 
     def null_dist(self, iters:int=2000):
-        return np.array(null_distribution(graph=self.G, 
+        self.null_betti = np.array(null_distribution(graph=self.G, 
                                            coloring=self.labels, 
                                            iterations=iters))
+        
+        return self.null_betti
 
     def pval(self, iters:int=2000):
-        obs = self.observed_betti()
-        null_dist = self.null_dist(iters=iters)
+            
+        if not self.p:
+            obs = self.observed_betti() if not self.obs_betti else self.obs_betti
+            null_dist = self.null_dist(iters=iters) if not self.null_betti else self.null_betti
 
-        p, _ , _ = calculate_p_value(obs, null_dist)
+            p, obsD2 , nullD2 = calculate_p_value(obs, null_dist)
 
-        return p
+            self.p = p
+            self.obsD2 = obsD2
+            self.nullD2 = nullD2
+
+            return p, obsD2, nullD2
+        
+        else:
+            return self.p, self.obsD2, self.nullD2
 
 
